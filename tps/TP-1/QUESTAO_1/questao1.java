f136abbb9a62e1b08e0738c9e9265392cbc9980b
caio <caiovitorgg@hotmail.com>
2026-08-17 17:43:33 -0300

    atualização atividade funcional

--- tps/TP-1/QUESTAO_1/questao1.java
+++ tps/TP-1/QUESTAO_1/questao1.java
@@ -1,4 +1,4 @@
-package QUESTAO_1;
+
 import java.util.Scanner;
 public class questao1{
     public static String CifradeCesar(String frase){
@@ -13,7 +13,7 @@ public class questao1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String frase = sc.nextLine();
-        while(!frase.equals("FIM")){
+        while(!(frase.length()==3 && frase.charAt(0)=='F' && frase.charAt(1)=='I' && frase.charAt(2)=='M')){
             String cifra = CifradeCesar(frase);
             System.out.println(cifra);
             frase=sc.nextLine();
