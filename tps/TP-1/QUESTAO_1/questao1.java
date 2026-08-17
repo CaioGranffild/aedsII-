3fca62a22f56d3a32cf49bae817d32b73ee0fb85
caio <caiovitorgg@hotmail.com>
2026-08-17 16:08:55 -0300

    first commit

---
+++ tps/TP-1/QUESTAO_1/questao1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class questao1{
+    public static String CifradeCesar(String frase){
+        String cifra = "";
+        for(int i = 0; i < frase.length(); i++){
+            char caractere = frase.charAt(i);
+            caractere+=3;
+            cifra += caractere;
+        }
+        return cifra;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String frase = sc.nextLine();
+        while(!frase.equals("FIM")){
+            String cifra = CifradeCesar(frase);
+            System.out.println(cifra);
+            frase=sc.nextLine();
+        }
+        sc.close();
+    }
+}
